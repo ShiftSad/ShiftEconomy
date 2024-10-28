3344b2e04df4460d9586e95d4900267d1c3c3bc2
Sadness <rosalimavictor@gmail.com>
2024-10-28 01:24:56 -0300

    TransactionRepository

--- proxy/src/main/java/codes/shiftmc/shiftEconomyProxy/repository/TransactionRepository.java
+++ proxy/src/main/java/codes/shiftmc/shiftEconomyProxy/repository/TransactionRepository.java
@@ -1,4 +1,42 @@
 package codes.shiftmc.shiftEconomyProxy.repository;
 
-public class TransactionRepository {
+import codes.shiftmc.shiftEconomyProxy.model.Transaction;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+public interface TransactionRepository {
+
+    /**
+     * Saves a new transaction record.
+     *
+     * @param transaction The Transaction to save.
+     * @return A Mono emitting the saved Transaction.
+     */
+    Mono<Transaction> save(Transaction transaction);
+
+    /**
+     * Finds all transactions where the specified user is either the sender or receiver.
+     *
+     * @param uuid The UUID of the user.
+     * @return A Flux emitting each Transaction involving the user.
+     */
+    Flux<Transaction> findByUser(UUID uuid);
+
+    /**
+     * Finds all transactions for a given receiver UUID.
+     *
+     * @param receiverUuid The UUID of the transaction receiver.
+     * @return A Flux emitting each Transaction received by the user.
+     */
+    Flux<Transaction> findByReceiverUuid(UUID receiverUuid);
+
+    /**
+     * Finds all transactions for a given sender UUID.
+     *
+     * @param senderUuid The UUID of the transaction sender.
+     * @return A Flux emitting each Transaction sent by the user.
+     */
+    Flux<Transaction> findBySenderUuid(UUID senderUuid);
 }
