c52ca69b8c4edbcbad61989ec9ecb455cd234390
Sadness <rosalimavictor@gmail.com>
2024-11-11 19:17:02 -0300

    Format for scientific numbers

--- common/src/main/java/codes/shiftmc/common/util/NumberFormatter.java
+++ common/src/main/java/codes/shiftmc/common/util/NumberFormatter.java
@@ -6,10 +6,12 @@ public final class NumberFormatter {
 
     private static final String[] suffixes;
     private static final DecimalFormat decimal;
+    private static final DecimalFormat scientific;
 
     static {
         suffixes = new String[]{"", "K", "M", "B", "T", "Q", "QQ", "S", "SS", "O", "N", "D", "UN", "DD"};
         decimal = new DecimalFormat("#.##");
+        scientific = new DecimalFormat("0.##E0");
     }
 
     public static String format(double number) {
@@ -18,10 +20,10 @@ public final class NumberFormatter {
 
         final var size = Math.log10(number);
 
-        if (size < 3) return String.valueOf(number);
+        if (size < 3) return decimal.format(number);
 
         final int index = (int) Math.floor(size / 3);
-        if (index >= suffixes.length) return String.valueOf(number); // Number way too big to format
+        if (index >= suffixes.length) return scientific.format(number); // Number way too big to format
 
         final double scaledNumber = number / Math.pow(10, index * 3);
         return decimal.format(scaledNumber) + suffixes[index];
