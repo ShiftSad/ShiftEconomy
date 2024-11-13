032de9f5d1603d1c55521a6a9f46adcf5688210a
Sadness <rosalimavictor@gmail.com>
2024-11-13 17:10:02 -0300

    PaymentPacketListener handler

--- plugin/src/main/java/codes/shiftmc/shiftEconomy/packet/PaymentPacketListener.java
+++ plugin/src/main/java/codes/shiftmc/shiftEconomy/packet/PaymentPacketListener.java
@@ -2,7 +2,11 @@ package codes.shiftmc.shiftEconomy.packet;
 
 import codes.shiftmc.common.messaging.PacketListener;
 import codes.shiftmc.common.messaging.packet.PaymentPacket;
+import codes.shiftmc.common.util.NumberFormatter;
+import codes.shiftmc.shiftEconomy.language.LanguageManager;
 import lombok.AllArgsConstructor;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
+import org.bukkit.Bukkit;
 
 @AllArgsConstructor
 public class PaymentPacketListener implements PacketListener<PaymentPacket> {
@@ -14,7 +18,13 @@ public class PaymentPacketListener implements PacketListener<PaymentPacket> {
 
     @Override
     public void onPacketReceived(PaymentPacket packet) {
-        // Handle the payment logic here
-        System.out.println("Received payment from " + packet.senderUUID() + " to " + packet.receiverUUID() + " of amount " + packet.amount());
+        var lang = LanguageManager.instance();
+
+        var receiver = Bukkit.getPlayer(packet.receiverUUID());
+        if (receiver == null) return;
+
+        lang.sendMessage(receiver, "player.pay.received",
+                         Placeholder.unparsed("sender", receiver.getName()),
+                         Placeholder.unparsed("amount", NumberFormatter.format(packet.amount())));
     }
 }
